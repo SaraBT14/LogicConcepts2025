0bc66da52e3ffa143fb659091d5bbda6f4a944b9
SaraBT <sara.bustamantet@udea.edu.co>
2025-03-16 13:26:46 -0500

    Complete exercise

---
+++ LogicConcepts/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,26 @@
+﻿do 
+{
+    Console.Write("Ingrese número entero o presione Ctrl + C para salir: ");
+    var nummberString = Console.ReadLine(); //"45"
+    var numberInt = 0;
+    if (int.TryParse(nummberString, out numberInt));
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberInt} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberInt} es impar");
+        }
+    }
+
+    else 
+    {
+        Console.WriteLine($"El número que ingresaste {nummberString} no es válido (número entero)");
+    }
+    
+
+    
+
+}while (true);
---
+++ LogicConcepts/LowerTriangularMatrix/LowerTriangularMatrix.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class LowerTriangularMatrix
+{
+    static void Main()
+    {
+        Console.Write("Ingrese orden de la matriz: ");
+        int N = int.Parse(s: Console.ReadLine());
+        int[,] matriz = new int[N, N];
+
+        // Llenar la matriz con la fórmula Celda[i][j] = i + j
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                matriz[i, j] = i + j;
+            }
+        }
+
+        // Imprimir la matriz completa
+        Console.WriteLine("Matriz completa:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Imprimir la triangular inferior
+        Console.WriteLine("Triangular inferior:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(matriz[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
